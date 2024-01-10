bc6e1d581afb16a6889e4f92702ed17d5d18d6dc
sanasharathchandra <93203011+sanasharathchandra@users.noreply.github.com>
2024-01-10 22:49:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ console.log(isSameType("3", 3));         // false
 }
 
 // do not change the code below.
-// let value1 = prompt("Enter Start of the Range.");
-// let value2 = prompt("Enter End Of the Range.");
+let value1 = prompt("Enter Start of the Range.");
+ let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
